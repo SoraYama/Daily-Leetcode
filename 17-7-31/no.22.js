4f92f61a591dd6ef69bfe25b5a361cfdca3a65c6
yanqi.hou <yanqi.hou@anansimobile.org>
2017-12-25 15:43:22 +0800

    update

--- 17-7-31/no.22.js
+++ 17-7-31/no.22.js
@@ -19,3 +19,41 @@ var generateParenthesis = function(n) {
   backTrack(res, "", 0, 0, n);
   return res;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let generateParenthesis1 = (n) => {
+  var list = [];
+  backtrack(list, '', 0, 0, n);
+  return list;
+}
+
+var backtrack = (list, str, open, close, max) => {
+  if(str.length === max * 2) {
+    list.push(str);
+    return;
+  }
+  if(open < max) {
+    backtrack(list, str + '(', open + 1, close, max);
+  }
+  if(close < open) {
+    backtrack(list, str + ')', open, close + 1, max);
+  }
+}
+
+console.log(generateParenthesis1(3));
